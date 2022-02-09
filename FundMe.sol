a4c536a69a48c2f92d11fd4c5090ac47233c6f86
PinkPigmyPuff <67124610+PinkPigmyPuff@users.noreply.github.com>
2022-02-09 08:48:15 -0800

    Create FundMe.sol

---
+++ FundMe.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+//video: 3:11:30
+pragma solidity ^0.6.0;
+
+// Get the latest ETH/USD price from chainlink price feed
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+contract FundMe {
+    using SafeMathChainlink for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        // $50
+        uint256 minimumUSD = 50 * 10 ** 18;
+        require(getConversionRate(msg.value) >= minimumUSD, "greedy mfer, send some more");
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (, int price,,,) = priceFeed.latestRoundData();
+            return uint256(price * 1000000000);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = ((ethPrice * ethAmount) / 1000000000000000000);
+        return ethAmountInUSD;
+    }
+
+    function withdraw() payable public {
+        msg.sender.transfer(address(this).balance);
+    }
+}
